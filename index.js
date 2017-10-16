a5c27bd31e947507ed245de3af710f3024970d8a
Julian Christov <julian.christov@live.com>
2017-10-16 16:13:53 +0200

    Added fix

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(process.env.PORT || 8080);
+
 var botgram = require("botgram");
 var bot = botgram(process.env.TELEGRAM_TOKEN);
 
