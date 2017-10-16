afc4cd4a853fb8748ce91f7bafc0c8c3774776be
Julian Christov <julian.christov@live.com>
2017-10-16 15:45:29 +0200

    2nd try

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// https://github.com/botgram/botgram
-// https://tutorials.botsfloor.com/creating-a-bot-using-the-telegram-bot-api-5d3caed3266d
-// https://blog.srnd.org/intro-to-node-js-making-a-telegram-bot-964b8cfe1129?gi=3b717c902aa9
+var http = require("http");
 
-const botgram = require("botgram");
-const bot = botgram("425567866:AAHQZ7NrvcvbLukOeFC71Dgbe3FjVfVes_s");
-
-bot.command("start", "help", (msg, reply) =>
-    reply.text("To schedule an alert, do: /alert <seconds> <text>"));
-
-bot.command("alert", (msg, reply) => {
-    var [seconds, text] = msg.args(2)
-    if (!seconds.match(/^\d+$/) || !text) return next();
-    setTimeout(() => reply.text(text), Number(seconds) * 1000);
-})
-
-bot.command((msg, reply) => reply.text("Invalid command."));
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(80);
+console.log('Server running at http://127.0.0.1:80/');
