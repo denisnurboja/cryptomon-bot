b89925d2050eabbbbd71c698c21267dc9eaece61
Julian Christov <julian.christov@live.com>
2017-10-17 00:17:11 +0200

    Added more commands

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@
 //    https://blog.srnd.org/intro-to-node-js-making-a-telegram-bot-964b8cfe1129?gi=3b717c902aa9
 //    http://mvalipour.github.io/node.js/2015/11/10/build-telegram-bot-nodejs-heroku
 //    http://qpbp.name/tutorials/2016/07/13/deploying-telegram-bot-to-heroku.html
+//    https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json
+//    http://www.unicode.org/emoji/charts/full-emoji-list.html
 
 const TeleBot = require('telebot');
 const ccxt = require('ccxt');
@@ -27,17 +29,14 @@ bot.on(['/start'], (msg) => {
     //    ['/alarm', '/monitor', '/help']
     //], { resize: true, once: false });
     const replyMarkup = bot.inlineKeyboard([
-        [ bot.inlineButton('💱 Exchanges', {callback: '/exchanges'}) ],
-        [ bot.inlineButton('⏰ Alarm', {callback: '/alarm'}) ],
-        [ bot.inlineButton('📢 Signal', {callback: '/signal'}) ],
-        [ bot.inlineButton('ℹ️ Help', {callback: '/help'}) ],
-        [ bot.inlineButton('❓ About', {callback: '/about'}) ]
+        [ bot.inlineButton('💱 Exchanges', {callback: '/exchanges'}), bot.inlineButton('💲 Coins', {callback: '/coins'}) ],
+        [ bot.inlineButton('⏰ Alarm', {callback: '/alarm'}), bot.inlineButton('📢 Signal', {callback: '/signal'}) ],
+        [ bot.inlineButton('ℹ️ Help', {callback: '/help'}), bot.inlineButton('❓ About', {callback: '/about'}) ]
     ]);
     bot.sendMessage(msg.from.id, '<b>Available commands:</b>', { parseMode: 'HTML', replyMarkup });
     return bot.sendMessage(msg.from.id, `Hi, <b>${ msg.from.first_name }</b>!`, { parseMode: 'HTML' });
     // return bot.sendMessage(msg.from.id, '😺 Use commands: /alarm, /monitor, /about and /help', { parseMode: 'HTML', replyMarkup });
 });
-//bot.on('text', (msg) => msg.reply.text(msg.text));
 
 bot.on('/about', (msg) => {
     return bot.sendMessage(msg.from.id, '<b>About</b> not implemented yet', { parseMode: 'HTML' });
@@ -56,10 +55,38 @@ bot.on(['/signal', '/s'], (msg) => {
 });
 
 bot.on(['/exchanges', '/e'], (msg) => {
-    var exc = ccxt.exchanges.join(', ');
+    const replyMarkup = bot.inlineKeyboard([
+        [ bot.inlineButton('Bittrex', {callback: '/exchange bittrex'}), bot.inlineButton('Bitfinex', {callback: '/exchange bitfinex'}) ],
+        [ bot.inlineButton('Binance', {callback: '/exchange binance'}), bot.inlineButton('GDAX', {callback: '/exchange gdax'}) ]
+    ]);
+    //var exc = ccxt.exchanges.join(', ');
+    var exc = '';
     //console.log(exc);
-    return bot.sendMessage(msg.from.id, `<b>Exchanges:</b> ${ exc }`, { parseMode: 'HTML' });
+    return bot.sendMessage(msg.from.id, `<b>Exchanges:</b> ${ exc }`, { parseMode: 'HTML', replyMarkup });
 });
+
+bot.on(['/coins', '/c'], (msg) => {
+    const replyMarkup = bot.inlineKeyboard([
+        [ bot.inlineButton('Bitcoin', {callback: '/coin BTC'}), bot.inlineButton('Ethereum', {callback: '/coin ETH'}) ],
+        [ bot.inlineButton('Dash', {callback: '/coin DASH'}), bot.inlineButton('NEO', {callback: '/coin NEO'}) ]
+    ]);
+    //var exc = ccxt.exchanges.join(', ');
+    //console.log(exc);
+    coins = '';
+    return bot.sendMessage(msg.from.id, `<b>Coins:</b> ${ coins }`, { parseMode: 'HTML', replyMarkup });
+});
+
+bot.on(/^\/exchange (.+)$/, (msg, param) => {
+    const exchange = param.match[1];
+    return bot.sendMessage(msg.from.id, `<b>Exchange:</b> ${ exchange }`, { parseMode: 'HTML' });
+});
+
+bot.on(/^\/coin (.+)$/, (msg, param) => {
+    const coin = param.match[1];
+    return bot.sendMessage(msg.from.id, `<b>Coin:</b> ${ coin }`, { parseMode: 'HTML' });
+});
+
+bot.on('text', (msg) => msg.reply.text(`📣 ${ msg.text }`));
 
 bot.start();
 
