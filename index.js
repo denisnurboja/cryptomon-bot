5e898c075b2f7c2398d49df240917e52e970229c
Julian Christov <julian.christov@live.com>
2017-10-16 15:48:47 +0200

    3rd try

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ var http = require("http");
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(process.env.PORT || 8080);
