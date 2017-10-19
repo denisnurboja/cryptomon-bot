f6bf56f50fe3b0fda82b6e8ff4d8cf3f17eee2fc
Julian Christov <julian.christov@live.com>
2017-10-19 21:38:09 +0200

    Added keepalive hack

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ require('./db');
 require('./bot');
 require('./web');
 require('./ccxt');
+
+// Keepalive hack
+var http = require("http");
+setInterval(function() {
+    http.get("http://cryptomon-bot.herokuapp.com");
+}, 15 * 60000); // every 15 minutes
